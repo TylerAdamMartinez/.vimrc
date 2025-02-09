b553dce1e8dc98e5a0e3f114a6dd3021491258bb
Tyler Adam Martinez <tylermartinez@my.unt.edu>
2025-02-08 23:24:56 -0600

    [config.fish] Add alias for git & yazi

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,29 +1,12 @@
 # ~/.config/fish/config.fish
 
-# Add Homebrew to PATH
-if test -d /opt/homebrew/bin
-    set -Ux HOMEBREW_PREFIX /opt/homebrew
-    set -Ux HOMEBREW_CELLAR /opt/homebrew/Cellar
-    set -Ux HOMEBREW_REPOSITORY /opt/homebrew
-    set -Ux PATH /opt/homebrew/bin /opt/homebrew/sbin $PATH
-    set -Ux MANPATH /opt/homebrew/share/man $MANPATH
-    set -Ux INFOPATH /opt/homebrew/share/info $INFOPATH
-end
-
-# Add Cargo to PATH
-fish_add_path $HOME/.cargo/bin
-
 # Initialize Homebrew (important for some commands)
 eval (/opt/homebrew/bin/brew shellenv)
 
-set -Ux HELIX_RUNTIME $HOME/src/helix/runtime
-
-# Ensure zoxide works in Fish
 if type -q zoxide
     zoxide init fish | source
 end
 
-# Ensure starship works in Fish
 if type -q starship
     starship init fish | source
 end
@@ -32,7 +15,22 @@ function ll
     exa -laG --icons --classify --git --all --group-directories-first
 end
 
-# Run neofetch (only if installed) on startup
+function g
+    git $argv
+end
+
+complete -c g -w git
+
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
+
+# Run neofetch on startup
 if type -q neofetch
     neofetch
 end
