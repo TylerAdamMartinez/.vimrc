c69053d71a951015d2f40916a0ec171b926f860c
Tyler Adam Martinez <tylermartinez@my.unt.edu>
2025-02-16 09:01:10 -0600

    [config-osx] init config

---
+++ .config/fish/config-osx.fish
@@ -0,0 +1,8 @@
+# ~/.config/fish/config-osx.fish
+
+# Initialize Homebrew (important for some commands)
+eval (/opt/homebrew/bin/brew shellenv)
+
+if type -q exa
+    alias ll "exa -laG --icons --classify --git --all --group-directories-first"
+end
